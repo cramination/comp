0a345487a0a7c828ad6e5b230cbd373db4f53e96
cramination <camnation@comcast.net>
2025-01-24 22:07:21 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite';
  
  export default defineConfig({
-        resolve: {
-          alias: {
-            '@': '/src', // Example alias
-          },
-        },
 
     server: {
 
